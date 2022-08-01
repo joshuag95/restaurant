bf400a95396fdd5c78c44a6fb3b343177ca0fa3f
Austin <acw0415@gmail.com>
2022-08-01 15:35:05 -0400

    alot

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,11 +1,20 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 
-function RecipeCard({food}) {
+function RecipeCard({ food }) {
+    const { name, vegetarian, image, hasPeanuts, dairyFree, ingredients, comments, likes, favorite } = food
     console.log("3", food)
     return (
         <div>
-            <h1>{food.name}</h1>
+            <h3>{name}</h3>
+            <img src={image} style={{height: "120px"}}/>
+            <ul>
+                <li><a>{vegetarian ? "🍖: No" : "🍖: Yes"}</a></li>
+                <li><a>{hasPeanuts ? "🥜: Yes" : "🥜: No"}</a></li>
+                <li><a>{dairyFree ? "🐮: No" : "🐮: Yes"}</a></li>
+                <li><a>😘:{" " + likes}</a></li>
+                <li><a>{comments}</a></li>
+            </ul>
         </div>
     )
 }
